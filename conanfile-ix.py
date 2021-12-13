275f027e361eb072019c7f747c0da10eabb6c27e
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-12-13 20:26:15 +0100

    Specified conan for target OS

---
+++ conanfile-ix.py
@@ -0,0 +1,13 @@
+from conans import ConanFile, CMake
+
+
+class TeckosConan(ConanFile):
+    settings = ["os", "compiler", "build_type", "arch"]
+    requires = ["nlohmann_json/3.10.4", "sigslot/1.2.1", "libdeflate/1.8", "zlib/1.2.11"]
+    generators = "cmake", "cmake_find_package", "json"
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.requires.add("mbedtls/3.0.0")
+        if self.settings.os == "Linux":
+            self.requires.add("openssl/1.1.1l")
---
+++ conanfile.py
@@ -0,0 +1,11 @@
+from conans import ConanFile, CMake
+
+
+class TeckosConan(ConanFile):
+    settings = ["os", "compiler", "build_type", "arch"]
+    requires = ["nlohmann_json/3.10.4", "sigslot/1.2.1", "cpprestsdk/2.10.18"]
+    generators = "cmake", "cmake_find_package", "json"
+
+    def configure(self):
+        if self.settings.os == "Linux":
+            self.requires.add("openssl/1.1.1l")
