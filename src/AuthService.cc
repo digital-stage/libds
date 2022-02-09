c276819e39440b6e02f846e385c1b662864ac532
Christof Ruch <christof.ruch@gmx.de>
2022-02-09 10:51:19 +0100

    [logging] Switch to spdlog instead of stderr. Cleanup constructor code.

--- src/AuthService.cc
+++ src/AuthService.cc
@@ -7,27 +7,30 @@
 #include <nlohmann/json.hpp>
 #include <iostream>
 
+#include "spdlog/spdlog.h"
+
 using namespace DigitalStage::Auth;
 
-std::future<bool> AuthService::verifyToken(const std::string &token) {
+AuthService::AuthService(const std::string &authUrl) : url_(authUrl) {
+}
+
+std::future<bool> AuthService::verifyToken(const std::string& token)
+{
   return std::async(std::launch::async, [this, &token] {
     return verifyTokenSync(token);
   });
 }
 
-[[maybe_unused]] bool AuthService::verifyTokenSync(const std::string &token) {
+[[maybe_unused]] bool AuthService::verifyTokenSync(const std::string& token)
+{
   auto header = teckos::Header();
   header.insert({"Authorization", "Bearer " + token});
   auto result = teckos::rest::Get(this->url_ + "/profile");
-  if (result.statusCode == 200) {
+  if(result.statusCode == 200) {
     return true;
   }
-  std::cerr << "Could not verify token, reason is " << result.statusCode << ": " << result.statusMessage << std::endl;
+  spdlog::error("Could not verify token (code {}), reason is {}", result.statusCode, result.statusMessage);
   return false;
-}
-
-AuthService::AuthService(const std::string &authUrl) {
-  this->url_ = authUrl;
 }
 
 std::future<std::string> AuthService::signIn(const std::string &email,
@@ -63,6 +66,6 @@ std::future<bool> AuthService::signOut(const std::string &token) {
   if (result.statusCode == 200) {
     return true;
   }
-  std::cerr << "Could not sign out, reason is " << result.statusCode << ": " << result.statusMessage << std::endl;
+  spdlog::error("Could not sign out (code {}), reason is {}", result.statusCode, result.statusMessage);
   return false;
 }
